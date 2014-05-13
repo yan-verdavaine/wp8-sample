86799d458405ae6bef83d38bf9a412d929e4403e
yan-verdavaine <yan.verdavaine@gmail.com>
2014-05-13 19:37:19 +0200

    er

---
+++ Imaging/WaterMarking/PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using PhoneApp1.watermarking;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var task = new PhotoChooserTask();
+            task.Completed += task_Completed;
+            task.Show();
+
+        }
+
+        void task_Completed(object sender, PhotoResult e)
+        {
+            if(e.TaskResult == TaskResult.OK)
+            {
+
+                var bmp = new WriteableBitmap(0,0);
+                bmp.SetSource(e.ChosenPhoto);
+
+
+                var bmpres = new WriteableBitmap(bmp.PixelWidth,bmp.PixelHeight);
+
+                var txt = new WindowsPhoneControl1();
+                txt.Text = txtInput.Text;
+                txt.FontSize = 36 * bmpres.PixelHeight / 480;
+                txt.Width = bmpres.PixelWidth;
+                txt.Height = bmpres.PixelHeight;
+
+                //should call Measure and  when uicomponent is not in visual tree
+                txt.Measure(new Size(bmpres.PixelWidth, bmpres.PixelHeight));
+                txt.Arrange(new Rect(0, 0, bmpres.PixelWidth, bmpres.PixelHeight));
+
+
+                bmpres.Render(new Image() { Source = bmp }, null);
+                bmpres.Render(txt, null);
+
+                bmpres.Invalidate();
+                display.Source = bmpres;
+
+            }
+
+
+        }
+
+      
+    }
+}
---
+++ Imaging/WaterMarking/PhoneApp1/watermarking/WindowsPhoneControl1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneApp1.watermarking
+{
+    public partial class WindowsPhoneControl1 : UserControl
+    {
+
+        public string Text
+        {
+            get { return txt.Text; }
+            set { txt.Text = value; txt2.Text = value; }
+        }
+        public double FontSize
+        {
+            get { return txt.FontSize; }
+            set { txt.FontSize = value; txt2.FontSize = value; }
+        }
+        public WindowsPhoneControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}

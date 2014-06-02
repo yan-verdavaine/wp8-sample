4561359809b9d1f3e31646c2ab518f84d6a39085
yan-verdavaine <yan.verdavaine@gmail.com>
2014-06-02 16:05:44 +0200

    pipeline manager draft

---
+++ Imaging/PipelineManager/PipelineManager/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PipelineManager.Resources;
+
+namespace PipelineManager
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}

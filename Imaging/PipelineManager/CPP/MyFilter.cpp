550fc303b2f027300401a2a77883189ace26aefb
yan-verdavaine <yan.verdavaine@gmail.com>
2014-06-03 14:25:20 +0200

    duplicate cpp and csharp library => problem between pipelinemanager and ImagingSdkTemplate

---
+++ Imaging/PipelineManager/CPP/MyFilter.cpp
@@ -0,0 +1,106 @@
+#include "pch.h"
+#include "MyFilter.h"
+
+//include use to acces IBuffer memory
+#include <wrl.h>
+#include <robuffer.h>
+
+//concurrency::create_async
+#include <ppltasks.h>
+
+using namespace CPP;
+
+MyFilter::MyFilter(double f)
+	:factor(f)
+{
+}
+
+
+MyFilter::~MyFilter(void)
+{
+	m_sourceBuffer = nullptr;
+	m_targetBuffer = nullptr;
+}
+
+Nokia::Graphics::Imaging::ICustomFilterResponse^ MyFilter::BeginProcessing(Nokia::Graphics::Imaging::ICustomFilterRequest^ request)
+{
+	if(m_sourceBuffer == nullptr || m_sourceBuffer->Capacity < request->SourceBufferLength)
+	{
+		m_sourceBuffer = ref new Windows::Storage::Streams::Buffer (request->SourceBufferLength);
+	}
+
+	m_sourceBuffer->Length = m_sourceBuffer->Capacity;
+
+	if(m_targetBuffer == nullptr || m_targetBuffer->Capacity < request->TargetBufferLength)
+	{
+		m_targetBuffer = ref new Windows::Storage::Streams::Buffer (request->TargetBufferLength);
+	}
+	
+	m_targetBuffer->Length = m_targetBuffer->Capacity;
+
+	return this;
+
+}
+		
+Windows::Foundation::IAsyncAction^ MyFilter::PrepareAsync()
+{
+	//if you don't need to load data, return nullptr
+	return nullptr;
+
+	//or use concurrency::create_async
+	return concurrency::create_async([this]()
+	{
+		//load you data here
+	});
+}
+
+namespace
+{
+	byte* GetPointerToPixelData(Windows::Storage::Streams::IBuffer^ pixelBuffer, unsigned int *length = nullptr)
+	{
+		if (length != nullptr)
+		{
+			*length = pixelBuffer ->Length;
+		}
+		// Query the IBufferByteAccess interface.
+		Microsoft::WRL::ComPtr< Windows::Storage::Streams::IBufferByteAccess> bufferByteAccess;
+		reinterpret_cast<IInspectable*>( pixelBuffer)->QueryInterface(IID_PPV_ARGS(&bufferByteAccess));
+
+		// Retrieve the buffer data.
+		byte* pixels = nullptr;
+		bufferByteAccess->Buffer(&pixels);
+		return pixels;
+	}
+
+	template<typename T>
+	byte range(T v)
+	{
+		return v<0 ? (byte)0 : v>255 ? (byte)255 : (byte)v+.5;
+	}
+}
+
+
+		
+void MyFilter::ProcessBlock(Nokia::Graphics::Imaging::CustomFilterBlockParameters blockParameters)
+{
+
+	auto sourceBuffer = GetPointerToPixelData(m_sourceBuffer);
+	auto targetBuffer = GetPointerToPixelData(m_targetBuffer);
+
+	for(int y =0;y< blockParameters.Height;++y)
+	{
+		int index = 4*blockParameters.SourceStartIndex + y* 4*blockParameters.SourcePitch;
+		for(int x =0;x< blockParameters.Width;++x, index += 4)
+		{
+
+			targetBuffer[index] =   range(factor*sourceBuffer[index]); //B
+			targetBuffer[index+1] = range(factor*sourceBuffer[index+1]); //G
+			targetBuffer[index+2] = range(factor*sourceBuffer[index+2]); ///R
+			targetBuffer[index+3] = sourceBuffer[index+3]; // A
+
+		}
+	}
+
+
+
+}
---
+++ Imaging/PipelineManager/CSharp/Recipe/RecipeTemplate.cs
@@ -0,0 +1,140 @@
+﻿using Nokia.Graphics.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Recipe
+{
+    public class RecipeTemplate : IImageProvider, IImageConsumer, IDisposable
+    {
+
+
+        WeakReference<IImageConsumer> pipelineBegin = null;
+        WeakReference<IImageProvider> pipelineEnd = null;
+        WeakReference<IImageProvider> pipelineSource = null;
+
+
+
+        public RecipeTemplate(IImageProvider source)
+        {
+            Source = source;
+        }
+
+
+        protected void SetPipelineBeginEnd(IImageConsumer begin, IImageProvider end)
+        {
+
+            IImageProvider source;
+
+            if (begin != null && pipelineSource != null && pipelineSource.TryGetTarget(out source))
+            {
+                begin.Source = source;
+            }
+
+            pipelineBegin = new WeakReference<IImageConsumer>(begin);
+            pipelineEnd = new WeakReference<IImageProvider>(end);
+        }
+
+
+        #region IImageProvider implementation :
+        public Windows.Foundation.IAsyncOperation<Bitmap> GetBitmapAsync(Bitmap bitmap, OutputOption outputOption)
+        {
+            IImageProvider provider = null;
+            if (pipelineEnd == null || !pipelineEnd.TryGetTarget(out provider))
+                return null;
+            else
+                return provider.GetBitmapAsync(bitmap, outputOption);
+        }
+
+        public Windows.Foundation.IAsyncOperation<ImageProviderInfo> GetInfoAsync()
+        {
+            IImageProvider provider = null;
+            if (pipelineEnd == null || !pipelineEnd.TryGetTarget(out provider))
+                return null;
+            else
+                return provider.GetInfoAsync();
+        }
+
+        public bool Lock(RenderRequest renderRequest)
+        {
+            IImageProvider provider = null;
+            if (pipelineEnd == null || !pipelineEnd.TryGetTarget(out provider))
+                return false;
+            else
+                return provider.Lock(renderRequest);
+
+        }
+
+        public Windows.Foundation.IAsyncAction PreloadAsync()
+        {
+            IImageProvider provider = null;
+            if (pipelineEnd == null || !pipelineEnd.TryGetTarget(out provider))
+                return null;
+            else
+                return provider.PreloadAsync();
+        }
+        #endregion
+
+
+
+        #region IImageConsumer implementation
+        public IImageProvider Source
+        {
+            get
+            {
+                IImageProvider source;
+
+                if (pipelineSource != null && pipelineSource.TryGetTarget(out source))
+                {
+                    return source;
+                }
+
+                return null;
+
+            }
+            set
+            {
+                pipelineSource = new WeakReference<IImageProvider>(value);
+                IImageConsumer begin;
+                if (pipelineBegin != null && pipelineBegin.TryGetTarget(out begin))
+                    begin.Source = value;
+            }
+        }
+        #endregion
+
+        #region IDipose implementation
+        //http://msdn.microsoft.com/library/system.idisposable.aspx
+
+        // Flag: Has Dispose already been called? 
+        bool disposed = false;
+
+        // Public implementation of Dispose pattern callable by consumers. 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        // Protected implementation of Dispose pattern. 
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                // Free any other managed objects here. 
+                pipelineBegin = null;
+                pipelineEnd = null;
+                pipelineSource = null;
+            }
+
+            // Free any unmanaged objects here. 
+            //
+            disposed = true;
+        }
+        #endregion
+    }
+}
